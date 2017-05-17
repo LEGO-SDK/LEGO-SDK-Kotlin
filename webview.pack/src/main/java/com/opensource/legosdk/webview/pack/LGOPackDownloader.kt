f5856ed64e65baa5ab51e1db5c2b6dae217b4290
Pony <cuiminghui@cuiminghuideiMac.local>
2017-05-17 21:32:51 +0800

    Add LGODownloader and RSA validation.

--- webview.pack/src/main/java/com/opensource/legosdk/webview/pack/LGOPackDownloader.kt
+++ webview.pack/src/main/java/com/opensource/legosdk/webview/pack/LGOPackDownloader.kt
@@ -2,10 +2,7 @@ package com.opensource.legosdk.webview.pack
 
 import android.util.Base64
 import com.opensource.legosdk.core.LGOCore
-import java.io.BufferedOutputStream
-import java.io.ByteArrayOutputStream
-import java.io.File
-import java.io.FileOutputStream
+import java.io.*
 import java.net.URI
 import java.net.URL
 import java.security.Key
@@ -15,6 +12,9 @@ import java.security.interfaces.RSAPublicKey
 import java.security.spec.X509EncodedKeySpec
 import java.util.*
 import javax.crypto.Cipher
+import java.util.logging.Level.SEVERE
+import java.util.logging.Logger
+
 
 /**
  * Created by cuiminghui on 2017/5/17.
@@ -38,12 +38,75 @@ class LGOPackDownloader {
     }
 
     fun updateFile(url: String) {
-        requestMD5(url)?.let {
-            print(it)
+        requestServerMD5(url)?.let { serverMD5 ->
+            requestLocalMD5(url)?.let { localMD5 ->
+                if (!serverMD5.equals(localMD5, true)) {
+                    return downloadFile(url)
+                }
+            }
+            return downloadFile(url)
         }
     }
 
-    fun requestMD5(url: String): String? {
+    fun requestLocalMD5(url: String): String? {
+        try {
+            cachePath(url)?.let {
+                File(it).takeIf { it.canRead() }?.let {
+                    FileInputStream(it)?.let {
+                        val byteArrayOutputStream = ByteArrayOutputStream()
+                        val data = ByteArray(2048)
+                        while (true) {
+                            val count = it.read(data, 0, 2048)
+                            if (count < 0) {
+                                break
+
+                            }
+                            byteArrayOutputStream.write(data, 0, count)
+                        }
+                        it.close()
+                        byteArrayOutputStream.close()
+                        val messageDigest = MessageDigest.getInstance("MD5")
+                        messageDigest.update(byteArrayOutputStream.toByteArray())
+                        val digest = messageDigest.digest()
+                        val sb = StringBuffer()
+                        for (b in digest) {
+                            sb.append(String.format("%02x", b))
+                        }
+                        return sb.toString()
+                    }
+                }
+
+            }
+            val uri = URI(url)
+            uri.path.split("/").last().takeIf { it.endsWith(".zip") }?.let { zipName ->
+                LGOCore.context?.assets?.open(zipName)?.let {
+                    val byteArrayOutputStream = ByteArrayOutputStream()
+                    val data = ByteArray(2048)
+                    while (true) {
+                        val count = it.read(data, 0, 2048)
+                        if (count < 0) {
+                            break
+
+                        }
+                        byteArrayOutputStream.write(data, 0, count)
+                    }
+                    it.close()
+                    byteArrayOutputStream.close()
+                    val messageDigest = MessageDigest.getInstance("MD5")
+                    messageDigest.update(byteArrayOutputStream.toByteArray())
+                    val digest = messageDigest.digest()
+                    val sb = StringBuffer()
+                    for (b in digest) {
+                        sb.append(String.format("%02x", b))
+                    }
+                    return sb.toString()
+                }
+            }
+        } catch (e: Exception) {}
+        return null
+    }
+
+    fun requestServerMD5(url: String): String? {
         val BUFFER_SIZE = 512
         val url = url + ".hash?_t=" + Date().time
         val publicKey = requestPublicKey(url)?.replace("-----BEGIN PUBLIC KEY-----", "")?.replace("-----END PUBLIC KEY-----", "")?.replace(Regex("\n"), "") ?: return null
@@ -96,6 +159,7 @@ class LGOPackDownloader {
                 if (file.exists()) {
                     file.delete()
                 }
+                mkdirs(file)
                 val bufferedOutputStream = BufferedOutputStream(FileOutputStream(file))
                 val data = ByteArray(2048)
                 while (true) {
@@ -108,7 +172,17 @@ class LGOPackDownloader {
                 }
                 bufferedOutputStream.close()
             }
-        } catch (e: Exception) {}
+        } catch (e: Exception) {
+            print(e)
+        }
+    }
+
+    private fun mkdirs(file: File) {
+        val parentFile = file.parentFile
+        if (!parentFile.exists()) {
+            mkdirs(parentFile)
+            parentFile.mkdir()
+        }
     }
 
 }
